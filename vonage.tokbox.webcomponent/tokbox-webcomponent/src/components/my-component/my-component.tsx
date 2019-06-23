142c790f11c3cda912b8291ee81316b3a67e8ea2
Max Mednikov <maksym.mednikov@vonage.com>
2019-06-23 17:36:01 +0300

    websockets fixed

--- vonage.tokbox.webcomponent/tokbox-webcomponent/src/components/my-component/my-component.tsx
+++ vonage.tokbox.webcomponent/tokbox-webcomponent/src/components/my-component/my-component.tsx
@@ -1,61 +1,17 @@
 import { Component, Prop, h } from '@stencil/core';
-import AccCore from  'opentok-accelerator-core';
+//import WebSocket from 'sc-ws';
 
-import  AnnotationAccPack from 'opentok-annotation';
-import ArchivingAccPack from 'opentok-archiving'
-import ScreenSharingAccPack from 'opentok-screen-sharing'
-import TextChatAccPack from 'opentok-text-chat'
-import OtLogging from 'opentok-solutions-logging'
+//import a from 'op'
+import OtClient from '@opentok/client'
 
 
-let otCore;
-
-// import Core from './ot-core/core';
-
-//import 'opentok-solutions-css';
-
-// import { format } from '../../utils/utils';
 const options = {
-  // A container can either be a query selector or an HTMLElement
-  // eslint-disable-next-line no-unused-vars
-  streamContainers: function streamContainers(pubSub, type) {
-    return {
-      publisher: {
-        camera: '#cameraPublisherContainer',
-        screen: '#screenPublisherContainer',
-      },
-      subscriber: {
-        camera: '#cameraSubscriberContainer',
-        screen: '#screenSubscriberContainer',
-      },
-    }[pubSub][type];
-  },
-  controlsContainer: '#controls',
-  packages: ['textChat', 'screenSharing', 'annotation', 'archiving'],
-  communication: {
-    callProperties: null, // Using default
-  },
-  textChat: {
-    name: ['David', 'Paul', 'Emma', 'George', 'Amanda'][Math.random() * 5 | 0], // eslint-disable-line no-bitwise
-    waitingMessage: 'Messages will be delivered when other users arrive',
-    container: '#chat',
-  },
-  screenSharing: {
-    extensionID: 'plocfffmbcclpdifaikiikgplfnepkpo',
-    annotation: true,
-    externalWindow: false,
-    dev: true,
-    screenProperties: null, // Using default
-  },
-  annotation: {
-
-  },
-  archiving: {
-    startURL: 'https://example.com/startArchive',
-    stopURL: 'https://example.com/stopArchive',
-  },
+    credentials: {
+    apiKey: "46347062",
+    sessionId: "1_MX40NjM0NzA2Mn5-MTU2MDc5NzE4MjE4OH5Lb3Yzbi8yWTAwQUFRclh1Uzh4ZWdyTUR-fg",
+    token: "T1==cGFydG5lcl9pZD00NjM0NzA2MiZzaWc9NjkwZWZlNGIxMDBiMTIxNGIwZTJmYmRiNDAyMDI3YmU1ZmY1MjZjMDpzZXNzaW9uX2lkPTFfTVg0ME5qTTBOekEyTW41LU1UVTJNRGM1TnpFNE1qRTRPSDVMYjNZemJpOHlXVEF3UVVGUmNsaDFVemg0WldkeVRVUi1mZyZjcmVhdGVfdGltZT0xNTYwNzk3MjUzJm5vbmNlPTAuNTYyMTgzNTQxNDkxMjg3MiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTYzMzg5MjUzJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9"
+  }
 };
-
 
 @Component({
   tag: 'my-component',
@@ -63,62 +19,56 @@ const options = {
   shadow: true
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-
-
-  private state = {
-    connected: false,
-    active: false,
-    publishers: null,
-    subscribers: null,
-    meta: null,
-    localAudioEnabled: true,
-    localVideoEnabled: true,
-  };
 
   @Prop() test: string;
 
+  session : OtClient.Session;
+
+  handleError(error) {
+    if (error) {
+      alert(error.message);
+    }
+  }
+ 
+  publisher = OT.initPublisher('publisher', {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  }, this.handleError);
+
+
   componentDidLoad() {
-      console.log('componentDidLoad ->');
-      otCore = new AccCore(options);  
-      otCore.connect().then(function() { this.updateState({ connected: true }); });
-      console.log('otCore created', otCore);
+      console.log("componentDidLoad");
+      var webSocket =  WebSocket;
+      console.log("componentDidLoad", webSocket);
+      console.log("OT supported", OtClient.checkSystemRequirements());
+
+     
+
+      console.log('session ->');
+      this.session = OtClient.initSession(options.credentials.apiKey, options.credentials.sessionId, {}); 
+      console.log('init session ->');
+      const publisher = this.publisher;
+      this.session.connect(options.credentials.token,(error)=>{
+          if (error) {
+            this.handleError(error);
+          } else {
+            this.session.publish(this.publisher, this.handleError);
+          }
+      }
+      );
+
+     
   }
 
-  updateState(updates) {
-    Object.assign(this.state, updates);
-    Object.keys(updates).forEach(update => this.updateUI(update));
-  };
-
-  updateUI = (update) => {
-    const { connected, active } = this.state;
-
-    switch (update) {
-      case 'connected':
-        if (connected) {
-          document.getElementById('connecting-mask').classList.add('hidden');
-          document.getElementById('start-mask').classList.remove('hidden');
-        }
-        break;
-      case 'active':
-        if (active) {
-          document.getElementById('cameraPublisherContainer').classList.remove('hidden');
-          document.getElementById('start-mask').classList.add('hidden');
-          document.getElementById('controls').classList.remove('hidden');
-        }
-        break;
-      case 'meta':
-        //updateVideoContainers();
-        break;
-      default:
-        console.log('nothing to do, nowhere to go');
-    }
-  };
- 
+  
   render() {
-    return <div>We are here!! <div id="appVideoContainer" class="App-video-container"></div> </div>;
+    return <div>We are here!! <div id="appVideoContainer" class="App-video-container"></div>
+      <div id="videos">
+          <div id="subscriber"></div>
+          <div id="publisher"></div>
+      </div> 
+    </div>;
   }
 
 

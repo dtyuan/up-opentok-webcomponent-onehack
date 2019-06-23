60695330c885ba8af86ce0e8cce8635fef96251c
Jon McDuffie <jon.mcduffie@vonage.com>
2019-06-24 00:49:00 +0300

    Fix the subscription and publish target elements

--- vonage.tokbox.webcomponent/tokbox-webcomponent/src/components/my-component/my-component.tsx
+++ vonage.tokbox.webcomponent/tokbox-webcomponent/src/components/my-component/my-component.tsx
@@ -133,9 +133,16 @@ export class MyComponent {
             }
         });
         this.oneToOneSession.on('streamCreated', (event) => {
-          const element = document.createElement('div');
-          this.screenPublishEl.appendChild(element);
-          this.oneToOneSession.subscribe(event.stream, element, { appendMode: 'append' } as SubscriberProperties);
+          const options: SubscriberProperties = { insertMode: 'append', width: '100%', height: '100%' };
+          if(event.stream.videoType === 'screen') {
+            const element = document.createElement('div');
+            this.screenShareEl.appendChild(element);
+            this.oneToOneSession.subscribe(event.stream, element, options);
+          } else {
+            const element = document.createElement('div');
+            this.subscriberEl.appendChild(element);
+            this.oneToOneSession.subscribe(event.stream, element, options);
+          }
         });
 
         // Process the event.data property, if there is any data.
@@ -151,7 +158,7 @@ export class MyComponent {
       // annotation: true,
       externalWindow: true,
       dev: true,
-      screenSharingContainer: this.screenShareEl,
+      screenSharingContainer: this.screenPublishEl,
       screenProperties: {
         insertMode: 'append',
         width: '100%',
@@ -183,10 +190,10 @@ export class MyComponent {
 
       <button onClick={() => {this.initiateSession('_jmcduffie')}}>Invite</button>
       <div id="videos">
-          <div ref={el => this.subscriberEl = el as HTMLElement}></div>
-          <div ref={el => this.publisherEl = el as HTMLElement}></div>
-          <div ref={el => this.screenShareEl = el as HTMLElement}></div>
-          <div ref={el => this.screenPublishEl = el as HTMLElement}></div>
+          <div id="subscriber" ref={el => this.subscriberEl = el as HTMLElement}></div>
+          <div id="publisher" ref={el => this.publisherEl = el as HTMLElement}></div>
+          <div id="screenShare" ref={el => this.screenShareEl = el as HTMLElement}></div>
+          <div id="screenPublish" ref={el => this.screenPublishEl = el as HTMLElement}></div>
           <button onClick={() => {this.shareScreen();}}>Screen Share</button>
           <div id="sub-screen-sharing-container"></div>
           <div id="chatContainer"  ref={el => this.chatEl = el as HTMLElement}></div>
